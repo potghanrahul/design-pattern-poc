66d7782a8cb70bdb716fb6b2923147c26c630d0c
potghanrahul <rahulsampatpotghan@gmail.com>
2026-03-19 14:48:13 +0530

    Changes for State design pattern

--- src/main/java/com/code/dp/behavioural/state/Perform.java
+++ src/main/java/com/code/dp/behavioural/state/Perform.java
@@ -8,21 +8,17 @@ package com.code.dp.behavioural.state;
  * State Pattern lets an object alter its behavior when its internal state changes
  */
 public class Perform<N extends Number> {
-    private final N op1;
-    private final N op2;
     private Operation<N> operation;
 
-    public Perform(Operation<N> operation, N op1, N op2) {
+    public Perform(Operation<N> operation) {
         this.operation = operation;
-        this.op1 = op1;
-        this.op2 = op2;
     }
 
     public void setOperation(Operation<N> operation) {
         this.operation = operation;
     }
 
-    public N doPerform() {
+    public N doPerform(N op1, N op2) {
         return operation.calculate(op1, op2);
     }
 }
